0c6a844bef23aa82af1caac77a21fb82144df32f
Peter Chavez <peter33chavez@gmail.com>
2021-06-06 18:55:41 -0700

    added header details

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import BackgroundDetail from '../Images/header-detail.svg';
+
 
 const Header = () => {
     return (
-        <div>
-            {/* background container */}
-            {/* background detail svg */}
-            {/* h1 with landing page quote */}
-            {/* little about myself container */}
-            {/* resume btn */}
-        </div>
+        <header>
+            <img src={BackgroundDetail}  alt="background detail" className="background-detail" />
+            <div className="quote-container">
+                <h1 className="quote">Sales Manager Turned Developer</h1>
+                <p className='about-summary'>Hello, I’m Peter! A Data Driven Developer who brings a Business perspective to develop, with my Companies best interests in mind. </p>
+            </div>
+            <button className="resume-btn">Download Resume</button>
+        </header>
     )
 }
 
